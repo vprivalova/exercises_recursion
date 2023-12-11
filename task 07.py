594e74594e3e6b8b6ed1e8bf2b6450a7971a5f7c
vprivalova <privalovaa.vv@gmail.com>
2023-12-12 01:30:16 +0700

    task 7

--- task 07.py
+++ task 07.py
@@ -1,7 +1,17 @@
 def nod(a, b):
     """
-
-    :param a:
-    :param b:
-    :return:
+    The function determines the greatest common divisor of the numbers a and b.
+    :param a: first natural number
+    :param b: second natural number
+    :return: the greatest common divisor of the numbers a and b
     """
+
+    if a == b:
+        return b
+    elif a > b:
+        return nod(a-b, b)
+    elif a < b:
+        return nod(a, b - a)
+
+
+print(nod(2460, 4420))
